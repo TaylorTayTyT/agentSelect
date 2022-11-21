cb59dd3b44e6d9115b0073c46a25c61ba897b65b
TaylorTayTyT <>
2022-11-21 05:16:41 +0000

    committing to new discord standards

--- main.py
+++ main.py
@@ -1,77 +1,104 @@
 import discord
+from discord.ext import commands
 from replit import db
 from keepAlive import keep_alive
-import random 
+import random
 import os
 
-client = discord.Client()
+intents = discord.Intents().all()
+client = commands.Bot(command_prefix=',', intents=intents)
+
 db["teamwin"] = 0
 db["teamloss"] = 0
 db["welcome"] = 0
-characters = ["astra", "breach", "brimstone", "chamber", "cypher", "jett", "kayo", "killjoy", "neon", "omen", "phoenix", "raze", "reyna", "sage", "skye", "sova", "viper", "yoru"]
+characters = [
+    "astra", "breach", "brimstone", "chamber", "cypher", "jett", "kayo",
+    "killjoy", "neon", "omen", "phoenix", "raze", "reyna", "sage", "skye",
+    "sova", "viper", "yoru"
+]
+
 
 #from geeksforgeeks website
 def myAtoi(string):
     res = 0
-  
+
     # Iterate through all characters of
     #  input string and update result
     for i in string:
         res = res * 10 + (int(i) - int('0'))
-  
+
     return res
+
 
 @client.event
 async def on_ready():
-	print("We have logged in as {0.user}".format(client))
+    print("We have logged in as {0.user}".format(client))
 
 
 @client.event
 async def on_message(message):
-	if message.author == client.user:
-		return
+    if message.author == client.user:
+        return
 
-	if message.content.startswith("$hello"):
-		await message.channel.send(message.author.name)
+    if message.content.startswith("$hello"):
+        await message.channel.send(message.author.name)
 
-	#chooses an absolutely random variable
-	elif message.content.startswith("$random"):
-		chooseRandomly = random.randint(0, 17)
-		if (chooseRandomly < 0) or (chooseRandomly >= 18):
-			await message.channel.send("Taylor did something wrong")
-			return
-		await message.channel.send(characters[chooseRandomly])
-		
-		await send_photo(message, chooseRandomly)
-		return
+    #chooses an absolutely random variable
+    elif message.content.startswith("$random"):
+        chooseRandomly = random.randint(0, 17)
+        if (chooseRandomly < 0) or (chooseRandomly >= 18):
+            await message.channel.send("Taylor did something wrong")
+            return
+        await message.channel.send(characters[chooseRandomly])
 
-	elif (message.author.name == "MTN" or message.author.name == "taylornguyen") and (message.content.startswith("you're welcome") or message.content.startswith("youre welcome")) :
-		f = open("ywcounter.txt", "r+")
-		value = myAtoi(f.readline()) + 1
-		f.seek(0)
-		f.truncate(0)
-		f.write(str(value))
-		f.close()
-		await message.channel.send("mtn has said youre welcome " + str(value) + " times")
-	
+        await send_photo(message, chooseRandomly)
+        return
+
+    elif message.content.startswith("$ara"):
+        await message.channel.send(file=discord.File('ara.mp4'),
+                                   content=".play file")
+
+    elif (message.author.name == "MTN" or message.author.name == "taylornguyen"
+          ) and (message.content.startswith("you're welcome")
+                 or message.content.startswith("youre welcome")):
+        f = open("ywcounter.txt", "r+")
+        value = myAtoi(f.readline()) + 1
+        f.seek(0)
+        f.truncate(0)
+        f.write(str(value))
+        f.close()
+        await message.channel.send("mtn has said youre welcome " + str(value) +
+                                   " times")
+
+    elif message.content.startswith("!reaction"):
+        breakup = message.content.split()
+        await message.channel.send(breakup[1] + " has approached " +
+                                   breakup[2])
+
+
 async def send_photo(message, chooseRandomly):
-	dir_name = "valorant agents/" + characters[chooseRandomly]
-	list = os.listdir(dir_name) # dir is your directory path
-	number_files = len(list)
+    dir_name = "valorant agents/" + characters[chooseRandomly]
+    list = os.listdir(dir_name)  # dir is your directory path
+    number_files = len(list)
 
-	if number_files <= 0:
-		await message.channel.send("someone hates " + characters[chooseRandomly] + " so there isn't a pic of them")
-		return
+    if number_files <= 0:
+        await message.channel.send("someone hates " +
+                                   characters[chooseRandomly] +
+                                   " so there isn't a pic of them")
+        return
 
-	constraint = (random.randint(0, 100) % number_files) + 1
-	pic_name = "valorant agents/" + characters[chooseRandomly] + "/" + str(constraint) + ".png"
-	await message.channel.send(file=discord.File(pic_name))
-	return
+    constraint = (random.randint(0, 100) % number_files) + 1
+    pic_name = "valorant agents/" + characters[chooseRandomly] + "/" + str(
+        constraint) + ".png"
+    await message.channel.send(file=discord.File(pic_name))
+    return
+
 
 async def choose_character(file, message):
-	file = open(file, "r")
-	for line in file: 
-		await message.channel.send(line) 
+    file = open(file, "r")
+    for line in file:
+        await message.channel.send(line)
+
 
 keep_alive()
-client.run()
+client.run("OTQyOTQxMjcwMjExNDU3MDk1.G19B_o.Xab3LvZsGvH91zkSDqTWyTKVhhRPxKq9YVXQMs")
